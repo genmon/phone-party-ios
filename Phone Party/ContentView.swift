9b1871d8421cd6c15fed2028d1dcb963bf447c95
Matt Webb <matt@interconnected.org>
2023-11-08 13:44:58 +0000

    Initial Commit

---
+++ Phone Party/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Phone Party
+//
+//  Created by Matt Webb on 08/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Phone Party/Phone_PartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phone_PartyApp.swift
+//  Phone Party
+//
+//  Created by Matt Webb on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Phone_PartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
