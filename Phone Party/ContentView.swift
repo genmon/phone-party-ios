6c513c613a10ec30ba0133126e8abb001ebb6bc5
Matt Webb <matt@interconnected.org>
2023-11-11 08:49:25 +0000

    Local circle tapping

--- Phone Party/ContentView.swift
+++ Phone Party/ContentView.swift
@@ -1,24 +1,79 @@
-//
-//  ContentView.swift
-//  Phone Party
-//
-//  Created by Matt Webb on 08/11/2023.
-//
-
 import SwiftUI
 
+struct CircleData {
+    var id = UUID()
+    var color: Color
+    var position: CGPoint
+}
+
 struct ContentView: View {
+    @State private var circles: [CircleData] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Gesture recognizer covers the entire screen
+            Color.clear
+                .contentShape(Rectangle())
+                .gesture(
+                    DragGesture(minimumDistance: 0)
+                        .onEnded { value in
+                            let newCircle = CircleData(color: Color.randomPastel(), position: value.location)
+                            circles.append(newCircle)
+                            removeCircleDelayed(circleID: newCircle.id)
+                        }
+                )
+
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+
+            // Render circles
+            ForEach(circles, id: \.id) { circle in
+                CircleView(circle: circle)
+                    .onDisappear {
+                        circles.removeAll { $0.id == circle.id }
+                    }
+            }
         }
-        .padding()
+    }
+
+    private func removeCircleDelayed(circleID: UUID) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            withAnimation {
+                circles.removeAll { $0.id == circleID }
+            }
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct CircleView: View {
+    let circle: CircleData
+
+    var body: some View {
+        Circle()
+            .fill(circle.color)
+            .frame(width: UIScreen.main.bounds.width * 0.2, height: UIScreen.main.bounds.width * 0.2)
+            .position(circle.position)
+            .transition(.opacity)
+            .allowsHitTesting(false) // Ignore touch events
+    }
+}
+
+extension Color {
+    static func randomPastel() -> Color {
+        let hue = Double.random(in: 0...1)
+        let saturation: Double = 0.6 // Pastel-like saturation
+        let brightness: Double = 0.9 // Bright enough to appear pastel
+        return Color(hue: hue, saturation: saturation, brightness: brightness)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
