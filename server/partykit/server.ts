2e8a981064fd763cc52e90c342a36b46a1e1a321
Matt Webb <matt@interconnected.org>
2023-11-12 10:53:26 +0000

    Comments

--- server/partykit/server.ts
+++ server/partykit/server.ts
@@ -2,6 +2,8 @@ import type * as Party from "partykit/server";
 
 // server.ts
 // https://phone-party.genmon.partykit.dev
+// Connect to a room like
+// wss://phone-party.genmon.partykit.dev/party/default-room-name
 export default class PhonePartyServer implements Party.Server {
   constructor(public party: Party.Party) {}
 
@@ -13,7 +15,7 @@ export default class PhonePartyServer implements Party.Server {
       websocket.send("pong");
       return;
     }
-
+    // Broadcast the message to all connections except the sender
     this.party.broadcast(message, [websocket.id]);
   }
 }
